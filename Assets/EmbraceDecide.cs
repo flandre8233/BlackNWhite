3564df35f61453f16fcde1d9eecbcd39e057af05
(T1145-1) <flandre8233@gmail.com>
2020-01-15 17:21:00 +0800

    no message

---
+++ Assets/EmbraceDecide.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmbraceDecide : SingletonMonoBehavior<EmbraceDecide>
+{
+    [SerializeField]
+    int minDecideVal;
+    [SerializeField]
+    int maxDecideVal;
+
+    [SerializeField]
+    float PingPongSpeed;
+
+    [SerializeField]
+    int DecideDiff;
+
+    float DecideVal;
+
+   public float GetDecideVal {
+        get {
+            return DecideVal;
+        }
+    }
+    float GetDecidePoint {
+        get {
+            return (maxDecideVal + minDecideVal)/2 ;
+        }
+    }
+
+    bool GetIsInDecideArea {
+        get {
+            return (DecideVal >= GetLeftSideDecideArea && DecideVal < GetRightSideDecideArea );
+        }
+    }
+
+    float GetLeftSideDecideArea {
+        get {
+            return GetDecidePoint - DecideDiff;
+        }
+    }
+    float GetRightSideDecideArea {
+        get {
+            return GetDecidePoint + DecideDiff;
+        }
+    }
+    bool isToLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float CurTime;
+    // Update is called once per frame
+    void Update()
+    {
+        CurTime += Time.deltaTime;
+        if (isToLeft)
+        {
+            DecideVal = Mathf.Lerp(minDecideVal, maxDecideVal, CurTime / PingPongSpeed);
+        }
+        else
+        {
+            DecideVal = Mathf.Lerp(maxDecideVal, minDecideVal, CurTime / PingPongSpeed);
+        }
+
+        if (CurTime >= PingPongSpeed)
+        {
+            CurTime = 0;
+            isToLeft = !isToLeft;
+        }
+    }
+}
---
+++ Assets/SliderViewUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SliderViewUI : MonoBehaviour
+{
+    [SerializeField]
+    Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = EmbraceDecide.instance.GetDecideVal/100;
+    }
+}
